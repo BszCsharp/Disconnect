8c4f1ff3ba7ae414277a04c2406c91f354361559
reinhold <reinhold@web.de>
2015-05-19 20:38:52 +0200

    Commit für Projektmappe

---
+++ Disconnect/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Disconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFillDataSet = new System.Windows.Forms.Button();
+            this.buttonSerialize = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFillDataSet
+            // 
+            this.buttonFillDataSet.Location = new System.Drawing.Point(36, 31);
+            this.buttonFillDataSet.Name = "buttonFillDataSet";
+            this.buttonFillDataSet.Size = new System.Drawing.Size(140, 34);
+            this.buttonFillDataSet.TabIndex = 0;
+            this.buttonFillDataSet.Text = "DataSet füllen";
+            this.buttonFillDataSet.UseVisualStyleBackColor = true;
+            this.buttonFillDataSet.Click += new System.EventHandler(this.buttonFillDataSet_Click);
+            // 
+            // buttonSerialize
+            // 
+            this.buttonSerialize.Location = new System.Drawing.Point(38, 231);
+            this.buttonSerialize.Name = "buttonSerialize";
+            this.buttonSerialize.Size = new System.Drawing.Size(138, 27);
+            this.buttonSerialize.TabIndex = 1;
+            this.buttonSerialize.Text = "Dataset serialisieren";
+            this.buttonSerialize.UseVisualStyleBackColor = true;
+            this.buttonSerialize.Click += new System.EventHandler(this.buttonSerialize_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(404, 131);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Deserialisieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 321);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSerialize);
+            this.Controls.Add(this.buttonFillDataSet);
+            this.Name = "Form1";
+            this.Text = "Disconnect";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFillDataSet;
+        private System.Windows.Forms.Button buttonSerialize;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Disconnect/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Disconnect
+{
+    public partial class Form1 : Form
+    {
+        DataSet dsBestellung = null;
+        OleDbConnection con;
+        OleDbDataAdapter adKunde = null;
+        OleDbDataAdapter adBestellung = null;
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            con = new OleDbConnection(Properties.Settings.Default.ConString);
+            con.Open();
+            dsBestellung = new DataSet();
+            adKunde = new OleDbDataAdapter("select * from tKunde", con);
+            adKunde.MissingSchemaAction = MissingSchemaAction.Add;
+            adBestellung = new OleDbDataAdapter("select * From tBestellung", con);
+            adBestellung.MissingSchemaAction = MissingSchemaAction.Add;
+        }
+
+        private void buttonFillDataSet_Click(object sender, EventArgs e)
+        {
+            adKunde.FillSchema(dsBestellung, SchemaType.Source, "Kunde");
+            adKunde.Fill(dsBestellung, "Kunde");
+            adBestellung.FillSchema(dsBestellung, SchemaType.Source, "Bestellung");
+            adBestellung.Fill(dsBestellung, "Bestellung");
+            dataGridView1.DataSource = dsBestellung.Tables["Kunde"];
+            
+        }
+
+        private void buttonSerialize_Click(object sender, EventArgs e)
+        {
+            dsBestellung.WriteXmlSchema("Bestellung3.xsd");
+            dsBestellung.WriteXml("Bestellung.xml");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dsBestellung.ReadXmlSchema("Bestellung3.xsd");
+            dsBestellung.ReadXml("Bestellung.xml");
+
+            dataGridView1.DataSource = dsBestellung.Tables["Kunde"];
+
+        }
+    }
+}
